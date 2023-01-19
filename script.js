18c699570a40b4e9321cbd1b245f1f14df239eb9
Darren Kim <dkim555@yahoo.com>
2023-01-19 14:50:15 -0800

    add addFunction

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-let addCount = document.getElementById('add')
+let addCount = document.getElementById('add');
+let subCount = document.getElementById('subtract');
+let count = 0
+
+addCount.onclick = function() {addFunction()}
+function addFunction() {
+    count +1;
+    document.getElementById('count').innerHTML= count += 1;
+}
+
+subCount.onclick = function() {subFunction()}
+function subFunction() {
+    count -1;
+    document.getElementById('count').innerHTML= count -= 1;
+}
